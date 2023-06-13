57575f3f26b405ab39e11d8cad08cf338ceb80cb
anchikai <hentaisuperstar86@gmail.com>
2023-06-12 22:24:23 -0400

    Added 7 new commands to edit properties

--- src/main/java/net/anchikai/server/ServerEvents.java
+++ src/main/java/net/anchikai/server/ServerEvents.java
@@ -1,6 +1,5 @@
 package net.anchikai.server;
 
-import net.anchikai.ServerControls;
 import net.anchikai.world.ModGameRuleRegistry;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.server.MinecraftServer;
@@ -8,39 +7,109 @@ import net.minecraft.server.PlayerManager;
 import net.minecraft.server.world.ServerWorld;
 
 public class ServerEvents {
-    private int serverSimulation = 10;
+    private boolean serverPvp = true;
+    private boolean serverOnlineMode = true;
+    private boolean serverFlight = false;
     private int serverView = 10;
+    private boolean serverPreventProxyConnections = false;
+    private int serverSimulation = 10;
+    private int serverPlayerIdleTimeout = 0;
+    private boolean serverEnforceWhitelist = false;
+    private boolean serverDifficultyLocked = false;
 
 
     public ServerEvents() {
         ServerTickEvents.START_SERVER_TICK.register(this::checkGameRules);
     }
 
-    /**
-     * Checks for when the gamerules added in the mod are updated. If they are, update the respective values in server.properties
-     */
-    private void checkGameRules(MinecraftServer minecraftServer) {
-        for (ServerWorld world: minecraftServer.getWorlds()) {
-            refreshDistances(minecraftServer.getPlayerManager(), world);
+    private void checkGameRules(MinecraftServer server) {
+        for (ServerWorld world: server.getWorlds()) {
+            refreshPvp(server, world);
+            refreshOnlineMode(server, world);
+            refreshFlight(server, world);
+            refreshPreventProxyConnections(server, world);
+            refreshDistances(server.getPlayerManager(), world);
+            refreshPlayerIdleTimeout(server, world);
+            refreshDifficultyLocked(server, world);
+            refreshEnforceWhitelist(server, world);
         }
     }
 
-    /**
-     * Update all players view and simulation distances to respect the new cap.
-     */
-    public void refreshDistances(PlayerManager playerList, ServerWorld world) {
+
+    private void refreshPvp(MinecraftServer server, ServerWorld world) {
+        boolean pvp = world.getGameRules().getBoolean(ModGameRuleRegistry.PVP);
+
+        if (serverPvp != pvp) {
+            serverPvp = pvp;
+            server.setPvpEnabled(pvp);
+        }
+    }
+
+    private void refreshOnlineMode(MinecraftServer server, ServerWorld world) {
+        boolean onlineMode = world.getGameRules().getBoolean(ModGameRuleRegistry.ONLINE_MODE);
+
+        if (serverOnlineMode != onlineMode) {
+            serverOnlineMode = onlineMode;
+            server.setOnlineMode(onlineMode);
+        }
+    }
+
+    private void refreshFlight(MinecraftServer server, ServerWorld world) {
+        boolean flight = world.getGameRules().getBoolean(ModGameRuleRegistry.FLIGHT);
+
+        if (serverFlight != flight) {
+            serverFlight = flight;
+            server.setFlightEnabled(flight);
+        }
+    }
+
+    private void refreshPreventProxyConnections(MinecraftServer server, ServerWorld world) {
+        boolean preventProxyConnections = world.getGameRules().getBoolean(ModGameRuleRegistry.PREVENT_PROXY_CONNECTIONS);
+
+        if (serverPreventProxyConnections != preventProxyConnections) {
+            serverPreventProxyConnections = preventProxyConnections;
+            server.setFlightEnabled(preventProxyConnections);
+        }
+    }
+
+    private void refreshDistances(PlayerManager playerList, ServerWorld world) {
         int simulation = world.getGameRules().getInt(ModGameRuleRegistry.SIMULATION_DISTANCE);
         int view = world.getGameRules().getInt(ModGameRuleRegistry.VIEW_DISTANCE);
 
         if (serverSimulation != simulation) {
             serverSimulation = simulation;
             playerList.setSimulationDistance(simulation);
-            ServerControls.LOGGER.info("Updated simulation distance.");
         }
         if (serverView != view) {
             serverView = view;
             playerList.setViewDistance(view);
-            ServerControls.LOGGER.info("Updated view distance.");
+        }
+    }
+
+    private void refreshPlayerIdleTimeout(MinecraftServer server, ServerWorld world) {
+        int playerIdleTimeout = world.getGameRules().getInt(ModGameRuleRegistry.PLAYER_IDLE_TIMEOUT);
+
+        if (serverPlayerIdleTimeout != playerIdleTimeout) {
+            serverPlayerIdleTimeout = playerIdleTimeout;
+            server.setPlayerIdleTimeout(playerIdleTimeout);
+        }
+    }
+
+    private void refreshEnforceWhitelist(MinecraftServer server, ServerWorld world) {
+        boolean enforceWhitelist = world.getGameRules().getBoolean(ModGameRuleRegistry.ENFORCE_WHITELIST);
+
+        if (serverEnforceWhitelist != enforceWhitelist) {
+            serverEnforceWhitelist = enforceWhitelist;
+            server.setEnforceWhitelist(enforceWhitelist);
+        }
+    }
+
+    private void refreshDifficultyLocked(MinecraftServer server, ServerWorld world) {
+        boolean difficultyLocked = world.getGameRules().getBoolean(ModGameRuleRegistry.DIFFICULTY_LOCKED);
+
+        if (serverDifficultyLocked != difficultyLocked) {
+            serverDifficultyLocked = difficultyLocked;
+            server.setDifficultyLocked(difficultyLocked);
         }
     }
 }
--- src/main/java/net/anchikai/world/ModGameRuleRegistry.java
+++ src/main/java/net/anchikai/world/ModGameRuleRegistry.java
@@ -6,10 +6,15 @@ import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.minecraft.world.GameRules;
 
 public class ModGameRuleRegistry {
-    public static final GameRules.Key<GameRules.IntRule> VIEW_DISTANCE =
-            GameRuleRegistry.register(ServerControls.MOD_ID + ".viewDistance", GameRules.Category.MISC, GameRuleFactory.createIntRule(10));
-    public static final GameRules.Key<GameRules.IntRule> SIMULATION_DISTANCE =
-            GameRuleRegistry.register(ServerControls.MOD_ID + ".simulationDistance", GameRules.Category.MISC, GameRuleFactory.createIntRule(10));
+    public static final GameRules.Key<GameRules.BooleanRule> PVP = GameRuleRegistry.register(ServerControls.MOD_ID + ".pvp", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(true));
+    public static final GameRules.Key<GameRules.BooleanRule> ONLINE_MODE = GameRuleRegistry.register(ServerControls.MOD_ID + ".onlineMode", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(true));
+    public static final GameRules.Key<GameRules.BooleanRule> FLIGHT = GameRuleRegistry.register(ServerControls.MOD_ID + ".flight", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(false));
+    public static final GameRules.Key<GameRules.IntRule> VIEW_DISTANCE = GameRuleRegistry.register(ServerControls.MOD_ID + ".viewDistance", GameRules.Category.MISC, GameRuleFactory.createIntRule(10));
+    public static final GameRules.Key<GameRules.BooleanRule> PREVENT_PROXY_CONNECTIONS = GameRuleRegistry.register(ServerControls.MOD_ID + ".preventProxyConnections", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(false));
+    public static final GameRules.Key<GameRules.IntRule> SIMULATION_DISTANCE = GameRuleRegistry.register(ServerControls.MOD_ID + ".simulationDistance", GameRules.Category.MISC, GameRuleFactory.createIntRule(10));
+    public static final GameRules.Key<GameRules.IntRule> PLAYER_IDLE_TIMEOUT = GameRuleRegistry.register(ServerControls.MOD_ID + ".playerIdleTimeout", GameRules.Category.MISC, GameRuleFactory.createIntRule(0));
+    public static final GameRules.Key<GameRules.BooleanRule> ENFORCE_WHITELIST = GameRuleRegistry.register(ServerControls.MOD_ID + ".enforceWhitelist", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(false));
+    public static final GameRules.Key<GameRules.BooleanRule> DIFFICULTY_LOCKED = GameRuleRegistry.register(ServerControls.MOD_ID + ".difficultyLocked", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(false));
 
 
     public static void register() {
